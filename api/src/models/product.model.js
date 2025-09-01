e29f9e853dd00573ddb8f56d55773cbc1ba3a351
Ritikgupta856 <ritikgupta856@gmail.com>
2025-09-02 00:18:22 +0530

    style: Refactor product model for improved readability and consistency

--- api/src/models/product.model.js
+++ api/src/models/product.model.js
@@ -1,36 +1,36 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
-const productSchema = mongoose.Schema({
-
-  name:{
-    type:String,
-    required:true
-  },
-  slug:{
-    type:String,
-    required:true,
-    unique:true
-  },
-  description:{
-    type:String,
-    required:true
-  },
-  image:{
-    type:String,
-    required:true
-  },
-  price:{
-    type:String,
-    required:true
-  },
-  category:{
-    type:String,
-    required:true
-  },
-},
+const productSchema = mongoose.Schema(
   {
-    timestamps:true
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  {
+    timestamps: true,
   }
-)
+);
 
-export const Product = mongoose.model('Product',productSchema)
+export const Product = mongoose.model("Product", productSchema);
