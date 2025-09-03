de46db34ddca28a08c66d4377d409dae4ee66af6
Ritikgupta856 <ritikgupta856@gmail.com>
2025-09-03 19:00:05 +0530

    fix: Correct raw body reference in stripe webhook event construction

--- api/src/controllers/stripe.controller.js
+++ api/src/controllers/stripe.controller.js
@@ -44,7 +44,7 @@ export const stripeWebhook = async (req, res) => {
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
   let event;
   try {
-    event = stripe.webhooks.constructEvent(req.rawBody, sig, endpointSecret);
+    event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
   } catch (err) {
     console.log(`Error message: ${err.message}`);
     res.status(400).send(`Webhook Error: ${err.message}`);
